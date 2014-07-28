ff18bfabfce032118594c62aba9d77214fe40317
Sunjay Varma <haimunt@yahoo.com>
2014-07-28 16:56:55 -0400

    Functions for getting board rows and columns

--- sudoku.c
+++ sudoku.c
@@ -26,10 +26,9 @@ SudokuBoard* newSudokuBoard() {
  *
  * Should always be freed after use.
  */
-SudokuBoard* sudokuPuzzle() {
-	SudokuBoard* board = malloc(sizeof(SudokuBoard));
+SudokuBoard* solvedSudokuPuzzle() {
 	// A presolved board to shuffle around
-	/*board->tiles = {
+	short board_tiles[BOARD_SIZE*BOARD_SIZE] = {
 		1, 2, 3,  4, 5, 6,  7, 8, 9,
 		4, 5, 6,  7, 8, 9,  1, 2, 3,
 		7, 8, 9,  1, 2, 3,  4, 5, 6,
@@ -41,7 +40,38 @@ SudokuBoard* sudokuPuzzle() {
 		3, 4, 5,  6, 7, 8,  9, 1, 2,
 		6, 7, 8,  9, 1, 2,  3, 4, 5,
 		9, 1, 2,  3, 4, 5,  6, 7, 8
-	};*/
-	//TODO: Finish this by shuffling rows and columns
+	};
+	//TODO: Shuffle rows and columns before assigning
+	SudokuBoard* board = malloc(sizeof(SudokuBoard));
+	for (int i = 0; i < BOARD_SIZE*BOARD_SIZE; i++) {
+		board->tiles[i] = board_tiles[i];
+	}
 	return board;
 }
+
+/**
+ * Returns an array of size BOARD_SIZE that represents a row of the board
+ *
+ * Should be freed after use
+ */
+short* getBoardRow(SudokuBoard* board, int row_i) {
+	short* row = malloc(sizeof(short)*BOARD_SIZE);
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		row[i] = board->tiles[BOARD_SIZE * row_i + i];
+	}
+	return row;
+}
+
+/**
+ * Returns an array of size BOARD_SIZE that represents a column of the board
+ *
+ * Should be freed after use
+ */
+short* getBoardColumn(SudokuBoard* board, int col_i) {
+	short* col = malloc(sizeof(short)*BOARD_SIZE);
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		col[i] = board->tiles[i * BOARD_SIZE + col_i];
+	}
+	return col;
+}
+
--- sudoku.h
+++ sudoku.h
@@ -7,7 +7,12 @@ typedef struct {
 	short tiles[BOARD_SIZE*BOARD_SIZE];
 } SudokuBoard;
 
+// Board creation
 SudokuBoard* newSudokuBoard();
-SudokuBoard* sudokuPuzzle();
+SudokuBoard* solvedSudokuPuzzle();
+
+// Board methods
+short* getBoardRow(SudokuBoard* board, int row_i);
+short* getBoardColumn(SudokuBoard* board, int col_i);
 
 #endif
