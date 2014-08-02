0aa615abc4f514da8f1267811eb61e60641533df
Sunjay Varma <haimunt@yahoo.com>
2014-08-02 13:50:36 -0400

    Removed board properties header file

--- boardproperties.h
+++
@@ -1,8 +0,0 @@
-#ifndef __BOARD_PROPS_DEFS
-#define __BOARD_PROPS_DEFS
-
-// The width/height of a single board and also the big board
-#define BOX_SIZE 3
-#define BOARD_SIZE BOX_SIZE*BOX_SIZE
-
-#endif
--- sudoku.h
+++ sudoku.h
@@ -3,7 +3,9 @@
 
 #include <stdbool.h>
 
-#include "boardproperties.h"
+// The width/height of a single board and also the big board
+#define BOX_SIZE 3
+#define BOARD_SIZE BOX_SIZE*BOX_SIZE
 
 typedef struct {
 	short* tiles[BOARD_SIZE];
