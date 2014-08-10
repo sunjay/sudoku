f9bb641546be259192b8eda3ac8b0465e8562594
Sunjay Varma <haimunt@yahoo.com>
2014-08-10 16:02:57 -0400

    Time averaging

--- timesolvesudoku.c
+++ timesolvesudoku.c
@@ -12,6 +12,10 @@
 #include "puzzlesolver.h"
 
 int main(int argc, char* argv[]) {
+	int totalPuzzles = 0;
+	int completed = 0;
+	double averageSolveTime = 0;
+	double maxTime = 0;
 	while (true) {
 		SudokuBoard* board = readBoard(stdin);
 		if (board == NULL) {
@@ -23,7 +27,6 @@ int main(int argc, char* argv[]) {
 			freeSudokuBoard(board);
 			continue;
 		}
-
 		drawSudokuBoard(board);
 
 		LARGE_INTEGER freq;
@@ -40,21 +43,39 @@ int main(int argc, char* argv[]) {
 		tDiff.QuadPart = tF.QuadPart - t0.QuadPart;
 		elapsedTime = tDiff.QuadPart / (double) freq.QuadPart;
 		resolution = 1.0 / (double) freq.QuadPart;
+		printf("\n");
 		printf("Your performance counter ticks %I64u times per second\n", freq.QuadPart);
 		printf("Resolution is %lf nanoseconds\n", resolution*1e9);
-		printf("Code under test took %lf sec\n", elapsedTime);
+		printf("Code took %lf sec\n", elapsedTime);
+		printf("\n");
+
+		// Take the running average
+		averageSolveTime = (averageSolveTime * totalPuzzles + elapsedTime)/(totalPuzzles + 1);
+		totalPuzzles++;
+
+		if (elapsedTime > maxTime) {
+			maxTime = elapsedTime;
+		}
 
 		if (solved == NULL) {
 			printf("No solution found.\n");
 			freeSudokuBoard(board);
 			continue;
 		}
+		else {
+			completed++;
+		}
+
 		drawSudokuBoard(solved);
 
 		if (solved != board) {
 			freeSudokuBoard(solved);
 		}
 		freeSudokuBoard(board);
+	}
+
+	if (totalPuzzles > 0) {
+		printf("Solved %d of %d puzzles (avg %f secs, max %f secs)\n", completed, totalPuzzles, averageSolveTime, maxTime);
 	}
 
 	return 0;
