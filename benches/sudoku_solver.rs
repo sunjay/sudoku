6c3fa8e463d0d90106ae04252691f56221a6ea41
Sunjay Varma <varma.sunjay@gmail.com>
2018-09-05 10:05:18 -0400

    More granular benchmarking

--- benches/sudoku_solver.rs
+++ benches/sudoku_solver.rs
@@ -4,9 +4,34 @@ extern crate criterion;
 extern crate sudoku;
 
 use std::io::{self, Cursor};
+use std::fmt;
 
-use criterion::{Criterion};
+use criterion::{Criterion, Bencher};
 use sudoku::{Sudoku, ReadError};
+
+/// Special input type so the benchmark is displayed nicely
+#[derive(Clone)]
+struct BenchInput(Vec<(usize, Sudoku)>);
+
+impl fmt::Debug for BenchInput {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Assumption: The difference between all consecutive items must be 1
+        assert!(self.0.windows(2).map(|a| a[1].0 - a[0].0).all(|diff| diff == 1));
+        write!(f, "Puzzle {} to {}", self.0.first().unwrap().0 + 1, self.0.last().unwrap().0 + 1)
+    }
+}
+
+impl<'a> From<&'a [(usize, Sudoku)]> for BenchInput {
+    fn from(items: &'a [(usize, Sudoku)]) -> Self {
+        BenchInput(items.into())
+    }
+}
+
+impl BenchInput {
+    fn iter_mut(&mut self) -> impl Iterator<Item=&mut (usize, Sudoku)> {
+        self.0.iter_mut()
+    }
+}
 
 criterion_main!(solver);
 
@@ -22,19 +47,20 @@ macro_rules! solver_bench_group {
             fn $name(c: &mut Criterion) {
                 // We only time the solving process here and do not check whether the solution is correct.
                 // Correctness is checked in the test suite.
-                let boards = read_all_boards(include_bytes!($file));
-                c.bench_function(stringify!($name), move |b| b.iter_with_setup(
-                    || {
-                        boards.clone()
-                    },
-                    |boards| {
-                        for (i, mut board) in boards.into_iter().enumerate() {
-                            board.clone().solve()
-                                .map_err(|_| format!("Unable to solve #{}", i))
-                                .unwrap();
-                        }
-                    }
-                ));
+                let boards: Vec<_> = read_all_boards(include_bytes!($file)).enumerate().collect();
+                let boards: Vec<BenchInput> = boards.chunks(20).map(Into::into).collect();
+                c.bench_function_over_inputs(concat!(stringify!($name), "_solve"),
+                    |b: &mut Bencher, boards| b.iter_with_setup(
+                        || boards.clone(),
+                        |mut boards: BenchInput| boards.iter_mut()
+                            .map(|(i, board)| {
+                                board.solve().map_err(|_| format!("Unable to solve #{}", i))
+                            })
+                            .collect::<Result<Vec<_>, _>>()
+                            .unwrap(),
+                    ),
+                    boards,
+                );
             }
         )*
     };
@@ -46,23 +72,24 @@ solver_bench_group! {
     longest10, "../samples/longest10.txt";
 }
 
-/// Read all the boards in a given string into Sudoku structs
-///
-/// We do this all at once instead of lazily because we don't want any unnecessary overhead
-/// in the benchmark.
-fn read_all_boards(data: &'static [u8]) -> Vec<Sudoku> {
-    let mut boards = Vec::new();
+fn read_all_boards(data: &'static [u8]) -> impl Iterator<Item=Sudoku> {
+    // FIXME: Eventually when Rust supports `yield`, this could be rewritten without the extra struct
+    let reader = Cursor::new(data);
+    BoardReader {reader}
+}
 
-    let mut reader = Cursor::new(data);
-    loop {
-        let board = match Sudoku::read_board(&mut reader) {
-            Ok(board) => Ok(board),
-            Err(ReadError::IOError(ref err)) if err.kind() == io::ErrorKind::UnexpectedEof => break,
+struct BoardReader<'a> {
+    reader: Cursor<&'a [u8]>,
+}
+
+impl<'a> Iterator for BoardReader<'a> {
+    type Item = Sudoku;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match Sudoku::read_board(&mut self.reader) {
+            Ok(board) => Ok(Some(board)),
+            Err(ReadError::IOError(ref err)) if err.kind() == io::ErrorKind::UnexpectedEof => Ok(None),
             Err(err) => Err(err),
-        }.expect("Error reading sudoku board");
-
-        boards.push(board);
+        }.expect("Error reading sudoku board")
     }
-
-    boards
 }
